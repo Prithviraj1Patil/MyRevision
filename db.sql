2af09df0da0d6ad805a1f21ce1767d7db579860b
Prithviraj <130498472+Prithviraj1Patil@users.noreply.github.com>
2023-05-17 15:37:51 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,18 @@
+create table Employee(empid int primary key,
+ename varchar(20),
+deptno int,
+mgrid int,
+constraint foreign key (mgrid) references manager(mgrid)
+);
 
+create table ProjectManager(
+mgrid int primary key,
+mname varchar(20),
+empid int ,
+deptno int,
+constrait foreign key empid references employee(empid)
+);
+
+create table Stakeholder(
+sid int primary key,
+sname varchar(20));
